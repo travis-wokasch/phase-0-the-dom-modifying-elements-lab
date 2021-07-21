740825de54d0b8ed8196fca169325dfdc7c3a21c
Travis <tjwokasch@gmail.com>
2021-07-21 16:38:28 -0600

    commit tree 85d0d8573c3bca447d351523ba61bedc50e704a6

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove()
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+let t = document.createTextNode('Travis is the champion')
+newHeader.append(t)
+//header.innerHTML = "<h1>YOUR-NAME is the champion</h1>"
